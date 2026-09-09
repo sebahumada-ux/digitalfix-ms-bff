dd8a2362353690975e07f035d62d5b62a0297fb3
sebai <sebaignaciokr20@gmail.com>
2026-09-09 19:11:43 -0300

    chore: configura estructura inicial del BFF

---
+++ src/main/java/cl/duoc/digitalfix/bff/DigitalfixMsBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.digitalfix.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalfixMsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalfixMsBffApplication.class, args);
+	}
+
+}
