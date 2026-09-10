9cfbf788d92e7871bca85a9b306d93cf0e17c530
sebai <sebaignaciokr20@gmail.com>
2026-09-10 20:58:09 -0300

    feat: configura seguridad JWT con Azure Entra ID

---
+++ src/main/java/cl/duoc/digitalfix/bff/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package cl.duoc.digitalfix.bff.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+}
